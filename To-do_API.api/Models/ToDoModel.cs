2933bc974af7ca23d26fa145ac455a41f31b3c3f
sindre092025-eng <sindre092025@gmail.com>
2025-12-04 13:46:26 +0100

    Few extra folders

---
+++ To-do_API.api/Models/ToDoModel.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Net.Http.Headers;
+
+namespace To_do_API.api.Models;
+
+public class ToDoModel(string title, string description, bool completion, DateTime deadline)
+{
+    public string? Title {get;set;} = title;
+    public string? Description {get;set;} = description;
+    public bool Completion {get;set;} = completion;
+    public DateTime Deadline {get;set;} = deadline;
+}
