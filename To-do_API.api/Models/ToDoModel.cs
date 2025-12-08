7a0585eb2d5ae0d6b17af976a1312e65b2f4445e
Dainius Kokolovas <dainius092025@gmail.com>
2025-12-08 12:10:30 +0100

    created ITodoRepository with basic methods

--- To-do_API.api/Models/ToDoModel.cs
+++ To-do_API.api/Models/ToDoModel.cs
@@ -1,15 +1,23 @@
+
 using System;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Net.Http.Headers;
+using TodoApi.Domain.Enums;
 
 namespace To_do_API.api.Models;
 
-public class ToDoModel(string title, string description, bool completion, DateTime deadline)
+public class ToDoModel(string title, string tasks, DateTime deadline, bool completion = false)
 {
-    public string? Title {get;set;} = title;
-    public string? Description {get;set;} = description;
-    public bool Completion {get;set;} = completion;
+    public  Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Title {get;set;} = title;
+
+    public string Tasks {get;set;} = tasks;
+
     public DateTime Deadline {get;set;} = deadline;
+
+    public bool Completion {get;set;} = completion;
+
     public TodoStatus Status { get; set; } = TodoStatus.Pending;// Property that stores the todo's status, which has default as "Pending" when a new TodoItem is created
 }
