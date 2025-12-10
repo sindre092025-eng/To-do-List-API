8a22073f745a59e2058ddc1bfec3e153ab596859
sindre092025-eng <sindre092025@gmail.com>
2025-12-10 12:05:55 +0100

    enums error message

--- To-do_API.api/TodoApi.Application/Interfaces/ITodoRepository.cs
+++ To-do_API.api/TodoApi.Application/Interfaces/ITodoRepository.cs
@@ -1,17 +1,18 @@
 using To_do_API.api.Models;
+using TodoApi.Application;
 
 namespace To_do_API.api.TodoApi.Application.Interfaces
 {
     // Defines a contract for how a Todo repository must behave.
     public interface ITodoRepository
     {
-        Task<List<TodoItem>> GetAllAsync();// this retrieves all todo items from the data store.
+        Task<List<TodoModel>> GetAllAsync();// this retrieves all todo items from the data store.
 
-        Task<TodoItem?> GetByIdAsync(int id);// this oune retrieves a single todo item by its ID, incase no item with that ID exists it return NULL.
+        Task<TodoModel> GetByIdAsync(int id);// this oune retrieves a single todo item by its ID, incase no item with that ID exists it return NULL.
 
-        Task<TodoItem> AddAsync(TodoItem item);// it adds a new todo item to the data store, it also returns the item after it has been saved.
+        Task<TodoModel> AddAsync(TodoModel item);// it adds a new todo item to the data store, it also returns the item after it has been saved.
 
-        Task<TodoItem?> UpdateAsync(TodoItem item);// it updates an existing todo item, returns the updated item, or returns a null if the item did not exist.
+        Task<TodoModel> UpdateAsync(TodoModel item);// it updates an existing todo item, returns the updated item, or returns a null if the item did not exist.
 
         Task<bool> DeleteAsync(int id);// this bit deletes a todo item by ID and returns true if successfully deleted, or false if not found.
     }
---
+++ To-do_API.api/TodoApi.Domain/Enums/ErrorCode.cs
@@ -0,0 +1,11 @@
+namespace To_do_API.api.TodoApi.Domain.Enums;
+
+public enum ErrorCode
+{
+    OK = 200,
+    BadRequest = 400,
+    Unauthorized = 401,
+    NotFound = 404,
+    ServerError = 500
+
+}
